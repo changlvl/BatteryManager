402b057da051442a2af52590352df55f459f7657
liumouren <313688973@qq.com>
2015-06-01 13:27:42 +0800

    排行榜出了算法和提醒语之外功能已经实现，包括进入应用程序去关闭应用程序，包括根据应用程序的类型和状态显示不用的flag

---
+++ app/src/main/java/com/example/morega03/batterymanager/Utils/ActivityUtils.java
@@ -0,0 +1,27 @@
+package com.example.morega03.batterymanager.Utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by Morega03 on 2015/6/1.
+ */
+public class ActivityUtils {
+
+
+    public static boolean isRunningApp(Context context,String packageName){
+        boolean isRunning = false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> list = am.getRunningTasks(100);
+        for (ActivityManager.RunningTaskInfo info : list) {
+            if (info.topActivity.getPackageName().equals(packageName) && info.baseActivity.getPackageName().equals(packageName)) {
+                isRunning = true;
+                // find it, break
+                break;
+            }
+        }
+        return isRunning;
+    }
+}
